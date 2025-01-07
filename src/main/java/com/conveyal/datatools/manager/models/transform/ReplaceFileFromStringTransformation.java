e350cbfad27359fca49525ce3b448bfdd0f7a7ab
aboivert <aboivert.sa@gmail.com>
2025-01-07 09:14:51 +0100

    init

---
+++ src/main/java/com/conveyal/datatools/manager/models/transform/ReplaceFileFromStringTransformation.java
@@ -0,0 +1,9 @@
+package com.conveyal.datatools.manager.models.transform;
+
+import com.conveyal.datatools.common.status.MonitorableJob;
+
+/**
+ * This feed transformation will replace a file in the target zip (table) with the provided csv data.
+ */
+public class ReplaceFileFromStringTransformation extends StringTransformation {
+}
